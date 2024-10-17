6d31332cd5a88a25b1bbf00f5414fa37dfaf93c4
Jonatan Missora <jonatanjmissora@gmail.com>
2024-10-17 16:55:59 -0300

    terminado menu

---
+++ app/assets/RightArrowSVG.tsx
@@ -0,0 +1,5 @@
+export default function RightArrowSVG({ className }: { className: string }) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className={`${className}`}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" /></svg>
+  )
+}
--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -1,7 +1,8 @@
 import { Sorted } from "@/app/utils/sort"
+import Rubro from "./Rubro"
 
 const RUBROS = {
-  "ragazzi": ["luz", "gas", "telefono"],
+  "ragazzi": ["luz", "gas"],
   "patricios": ["agua", "cable", "internet"],
   "jmolina": ["sindicato", "cochera"],
   "palihue": ["visa", "contador", "rentas"]
@@ -14,45 +15,10 @@ export default function Menu() {
   return (
     <section className="w-5/12 flex flex-col">
 
-      <div className="m-8 p-8 bg-red-100 rounded-lg shadow-xl">
-        {rubros.map(rubro => <Rubro key={rubro} name={rubro} sectores={RUBROS[rubro]} />)}
+      <div className="m-24 p-8 bg-red-100 rounded-lg shadow-xl">
+        {rubros.map(rubro => <Rubro key={rubro} rubro={rubro} sectores={RUBROS[rubro]} />)}
       </div>
 
     </section>
-  )
-}
-
-const Rubro = ({ name, sectores }: { name: string, sectores: string[] }) => {
-
-  const sortedSectores = Sorted(sectores)
-
-  return (
-    <div className="py-4 flex gap-4">
-      <span>{name} ({sectores.length})</span>
-      {
-        sectores.length !== 0
-        && (
-          <RubroForm name={name} sectores={sortedSectores} />
-        )
-      }
-
-    </div>
-  )
-}
-
-const RubroForm = ({ name, sectores }: { name: string, sectores: string[] }) => {
-  return (
-    <form>
-
-      <select name="rubro" id={`rubro-${name}`}>
-        <option value=""></option>
-        {sectores.map(sector => <option key={sector} value={sector}>{sector}</option>)}
-
-      </select>
-
-      <input type="text" />
-      <input type="date" />
-      <button type="submit">Agregar</button>
-    </form>
   )
 }
